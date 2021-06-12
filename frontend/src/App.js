08724e1987aede1ccdbc2c91cfe7cb23c3a844a6
Mayank Shrivastava <shrivastava.mayank21@gmail.com>
2021-06-12 18:39:31 -0400

    basic recipe view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route
-} from "react-router-dom"
+} from "react-router-dom";
+import HomePage from './components/HomePage';
+import RecipeList from './components/RecipeList';
 
-function App() {
+const App = () => {
 	return (
 		<Router>
 			<div className="App">
-				<header className="App-header">
-				</header>
-				<Route path="/intro">
-					<Intro />
-				</Route>
-				{/* <Route path="/intro">
-					<Intro />
-				</Route>
-				<Route path="/intro">
-					<Intro />
-				</Route>
-				<Route path="/intro">
-					<Intro />
-				</Route> */}
+				<Switch>
+					<Route
+						path="/"
+						exact
+						component={() => <RecipeList />}
+					/>
+					{/* <Route path="/intro">
+						<Intro />
+					</Route>
+					<Route path="/intro">
+						<Intro />
+					</Route>
+					<Route path="/intro">
+						<Intro />
+					</Route> */}
+				</Switch>
 			</div>
 		</Router>
 	);
-}
-
-function Intro() {
-	return <h2>The Intro page</h2>;
-}
+};
 
 export default App;
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@material-ui/core'
+
+const HomePage = () => {
+	return (
+		// <button />
+		<Button variant="contained">Hello World! </Button>
+		// <Box />
+	);
+};
+
+export default HomePage;
