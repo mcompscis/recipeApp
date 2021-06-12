e366e8e21b99e59cdb79e67a50073f2be6f63f78
Mayank Shrivastava <shrivastava.mayank21@gmail.com>
2021-06-12 16:38:43 -0400

    router skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<header className="App-header">
+				</header>
+				<Route path="/intro">
+					<Intro />
+				</Route>
+				{/* <Route path="/intro">
+					<Intro />
+				</Route>
+				<Route path="/intro">
+					<Intro />
+				</Route>
+				<Route path="/intro">
+					<Intro />
+				</Route> */}
+			</div>
+		</Router>
+	);
+}
+
+function Intro() {
+	return <h2>The Intro page</h2>;
 }
 
 export default App;
