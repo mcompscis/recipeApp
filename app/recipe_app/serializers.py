45fd638b1a4e09e03b53b826b8755cbc94238860
Denis <denis.o.shapiro@gmail.com>
2021-06-13 13:22:28 -0400

    delete recipe_app

--- app/recipe_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SampleTable
-
-class SampleTableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SampleTable
-        fields = ('id', 'name')
