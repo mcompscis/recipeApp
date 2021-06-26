de7b537d724c61e97ad42781c93d2e74324a43b9
Denis <denis.o.shapiro@gmail.com>
2021-06-26 13:05:44 -0400

    clean up relative paths to queries

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -5,14 +5,14 @@ from setup_tables.query_manager import exec_query
 import os
 
 def top_five(request):
-    queryPath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'milestone_1_queries/get_top_recipes.sql'))
+    queryPath = os.path.join(os.path.dirname( __file__ ), '..', 'milestone_1_queries/get_top_recipes.sql')
     with open(queryPath, 'r') as file:
         queryText = file.read()
     return JsonResponse(exec_query(queryText), safe=False)
 
 def recipe_detail(request, pk):
     if request.method == 'GET':
-        queryPath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'milestone_1_queries/return_specific_recipe_info.sql'))
+        queryPath = os.path.join(os.path.dirname( __file__ ), '..', 'milestone_1_queries/return_specific_recipe_info.sql')
         with open(queryPath, 'r') as file:
             queryText = file.read()
         return JsonResponse(exec_query(queryText, [pk]), safe=False)
--- app/setup_tables/utils.py
+++ app/setup_tables/utils.py
@@ -2,7 +2,7 @@ import json
 import os
 
 def get_db_creds():
-    path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'recipeApp/db_credentials.json'))
+    path = os.path.join(os.path.dirname( __file__ ), '..', 'recipeApp/db_credentials.json')
     with open(path, "rb") as f:
         db_creds = json.load(f)
         db_creds["database"] = db_creds["database_name"]
