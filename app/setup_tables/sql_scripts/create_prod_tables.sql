52550720cf0ef609d20af443bfeb44071ade92c8
Pranav Vadrevu <pranav.vadrevu363@gmail.com>
2021-07-01 19:48:09 -0400

    Change Prod Tables Schema for RecipeIngredient and other tables

--- app/setup_tables/sql_scripts/create_prod_tables.sql
+++ app/setup_tables/sql_scripts/create_prod_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS User (
     user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(65) NOT NULL,
-    avg_recipe_rating DECIMAL(3, 2),
+    avg_recipe_rating FLOAT,
     num_ratings_received INT NOT NULL DEFAULT 0,
     num_recipes_created INT NOT NULL DEFAULT 0
 );
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS Recipe (
     description TEXT,
     recipe_text TEXT NOT NULL,
     calories INT,
-    avg_rating DECIMAL(3, 2) DEFAULT NULL,
+    avg_rating FLOAT DEFAULT NULL,
     time_to_prepare INT,
     num_ratings INT NOT NULL DEFAULT 0,
     img_url TEXT,
@@ -43,8 +43,8 @@ CREATE TABLE IF NOT EXISTS Tag (
 CREATE TABLE IF NOT EXISTS RecipeIngredient (
     recipe_id INT NOT NULL,
     ingredient_id INT NOT NULL, 
-    quantity INT NOT NULL,
-    measurement_unit VARCHAR(255) NOT NULL,
+    quantity FLOAT DEFAULT NULL,
+    measurement_unit VARCHAR(255) DEFAULT NULL,
     PRIMARY KEY(recipe_id, ingredient_id),
     FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id) ON DELETE CASCADE,
     FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id) ON DELETE CASCADE
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS Interaction (
     user_id INT NOT NULL,
     recipe_id INT NOT NULL,
     interaction_date DATE NOT NULL,
-    rating DECIMAL(3, 2),
+    rating FLOAT,
     review TEXT,
     PRIMARY KEY(user_id, recipe_id),
     FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
