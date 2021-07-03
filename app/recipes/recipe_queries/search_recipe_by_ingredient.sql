a64564bca2ff81510d56ec074c7133d45d2b10a9
Pranav Vadrevu <pranav.vadrevu363@gmail.com>
2021-07-03 10:08:58 -0400

    Change SampleTable mentions to Prod Table mentions

--- app/recipes/recipe_queries/search_recipe_by_ingredient.sql
+++ app/recipes/recipe_queries/search_recipe_by_ingredient.sql
@@ -1,13 +1,14 @@
--- This query filters the SampleRecipe table such that only entries with specified ingredients and those without the specified allergens will be returned
+-- This query filters the Recipe table such that only entries with specified ingredients and those without the specified allergens will be returned
 -- The HAVING clause will be modified by the backend to include all filters
 -- The query below assumes that the user wants to filter the recipe list based on ingredients like 'chicken' and 'chilli powder'
 -- and wants to avoid the ingredient 'paneer'
+-- TODO: Change hardcoded ingredients to dynamic strings using %s
 SELECT R.recipe_id,
        R.recipe_name,
        GROUP_CONCAT(ingredient_name) AS "ingredients"
-FROM   SampleRecipe R,
-       SampleRecipeIngredient RI,
-       SampleIngredient I
+FROM   Recipe R,
+       RecipeIngredient RI,
+       Ingredient I
 WHERE  ( R.recipe_id = RI.recipe_id )
        AND ( RI.ingredient_id = I.ingredient_id )
 GROUP  BY R.recipe_id,
