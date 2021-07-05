cf9d81d0a1f113bb3825f2979ac191cc60f78e52
alric-j <alricbyju@gmail.com>
2021-07-05 16:06:17 -0400

    change ordering for filter query

--- app/recipes/recipe_queries/search_recipe_by_tags_and_cuisine.sql
+++ app/recipes/recipe_queries/search_recipe_by_tags_and_cuisine.sql
@@ -2,14 +2,17 @@
 -- The user passes %(tag_text)s . %(cuisine_name)s to filter the recipes
 -- The Frontend decides what %(limit_val)s and %(offset_val)s to load the appropriate number of pages at a time
 -- limit_val is number of recipes to display on a page and offset_val is the offset for recipes on each page
+-- The queries are ordered based on weighted rating
 
 SELECT recipe_id,
        recipe_name,
-       avg_rating
+       avg_rating,
+       num_ratings
 FROM   (SELECT tag_text,
                r.recipe_id AS recipe_id,
                recipe_name,
                avg_rating,
+               num_ratings,
                cuisine_name
         FROM   Tag AS t
                INNER JOIN RecipeTag AS rt
@@ -20,6 +23,6 @@ FROM   (SELECT tag_text,
                        ON r.cuisine_id = c.cuisine_id) 
 WHERE  tag_text = %(tag_text)s
        AND cuisine_name = %(cuisine_name)s
-ORDER  BY avg_rating DESC; 
+ORDER  BY ((avg_rating * num_ratings) + (SELECT AVG(avg_rating) FROM Recipe) * 100) / (num_ratings + 100) DESC; 
 LIMIT  %(limit_val)s
 OFFSET %(offset_val)s;
