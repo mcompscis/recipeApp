235f564dd25a9d1247c584e8fc13e67f4a0abd02
Pranav Vadrevu <pranav.vadrevu363@gmail.com>
2021-07-05 18:29:25 -0400

    Fix search_recipe_by_tags_and_cuisine.sql

--- app/recipes/recipe_queries/search_recipe_by_tags_and_cuisine.sql
+++ app/recipes/recipe_queries/search_recipe_by_tags_and_cuisine.sql
@@ -4,10 +4,11 @@
 -- limit_val is number of recipes to display on a page and offset_val is the offset for recipes on each page
 -- The queries are ordered based on weighted rating
 
-SELECT recipe_id,
+SELECT DISTINCT recipe_id,
        recipe_name,
        avg_rating,
-       num_ratings
+       num_ratings,
+       cuisine_name
 FROM   (SELECT tag_text,
                r.recipe_id AS recipe_id,
                recipe_name,
@@ -20,9 +21,9 @@ FROM   (SELECT tag_text,
                INNER JOIN Recipe AS r
                        ON rt.recipe_id = r.recipe_id
                INNER JOIN Cuisine AS c
-                       ON r.cuisine_id = c.cuisine_id) 
+                       ON r.cuisine_id = c.cuisine_id
 WHERE  tag_text = %(tag_text)s
-       AND cuisine_name = %(cuisine_name)s
-ORDER  BY ((avg_rating * num_ratings) + (SELECT AVG(avg_rating) FROM Recipe) * 100) / (num_ratings + 100) DESC; 
+AND cuisine_name = %(cuisine_name)s) T
+ORDER  BY ((avg_rating * num_ratings) + (SELECT AVG(avg_rating) FROM Recipe) * 100) / (num_ratings + 100) DESC
 LIMIT  %(limit_val)s
 OFFSET %(offset_val)s;
