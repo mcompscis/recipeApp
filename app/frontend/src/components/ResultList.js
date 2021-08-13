3b2578fe3fa892d3368d8c64e8e1888c6fe03e86
Denis <denis.o.shapiro@gmail.com>
2021-08-12 23:46:54 -0400

    create loading skeleton

--- app/frontend/src/components/ResultList.js
+++ app/frontend/src/components/ResultList.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import PreviewCard from './PreviewCard';
 import { makeStyles } from '@material-ui/core/styles';
+import { useSelector } from 'react-redux'
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles({
   flexbox: {
@@ -10,13 +12,28 @@ const useStyles = makeStyles({
     alignItems: "stretch",
     justifyContent: "center",
   },
+  root: {
+    margin: 10,
+    flex: "0 0 23.5%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    width: "15%%"
+  }
 });
 
 const ResultList = ({recipes}) => {
   const classes = useStyles()
+  let loading = useSelector(state => state.loading)
+
   return(
     <div className={classes.flexbox}>
-        {recipes.map(recipe =>
+        {loading ? 
+        [...Array(48).keys()].map(idx => 
+          <Skeleton key={idx} className={classes.root} variant="rect" width={261} height={346} />
+        )
+        :
+        recipes.map(recipe =>
           <PreviewCard recipe={recipe} key={recipe.recipe_id}/>
         )}
     </div>
